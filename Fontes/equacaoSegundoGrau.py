e73aa6688320e59b512e8ccd9975b4273ece5d02
ed1rac <edkallenn@gmail.com>
2016-06-29 16:56:02 -0500

    Funções Estatísticas e Equação do Segundo Grau

---
+++ Fontes/equacaoSegundoGrau.py
@@ -0,0 +1,23 @@
+"""
+Função:
+    Equação do segundo grau
+Autor:
+    Ed - Data: 20/05/2016 -
+Observações: COmplexidade: ?
+"""
+#Com anotações
+def eq2(a: int, b:int, c: int):
+    """Retorna raízes para equações de 2º grau"""
+    if a==0:
+        return -c/b
+    else:
+        d = (b**2 - 4*a*c)**0.5
+        x1 = (-b - d) / (2*a)
+        x2 = (-b + d) / (2*a)        
+        return x1,x2
+
+#testando a função
+print(eq2(1,-5,6))
+print(eq2(0,2,-2))
+print(eq2(2,2,0))
+print(eq2(2,0,2))
---
+++ Fontes/probabilidade_moeda.py
@@ -0,0 +1,54 @@
+# -*- coding: UTF-8 -*-
+"""
+Função:
+    Exemplo de lançamento de moeda
+Autor:
+    Professor Ed - Data: 29/05/2016 -
+Observações:  ?
+"""
+def gera_matriz_lancamentos(matriz, tamanho):
+	import random
+	matriz_faces = []
+	print 'Gerando...'
+	for x in range(tamanho):
+		num = random.randint(1,2) #1 = cara, 2 = coroa
+		matriz.append (num)
+		
+		if num==1:
+			matriz_faces.append('Cara')
+		else:
+			matriz_faces.append('Coroa')
+
+	print matriz_faces
+
+
+def calcula_probabilidades(matriz, tamanho):
+	soma_cara = 0
+	soma_coroa = 0
+	
+	for i in range(len(matriz)):
+		if matriz[i]==1:
+			soma_cara = soma_cara+1
+		elif matriz[i]==2:
+			soma_coroa = soma_coroa + 1
+	
+	probabilidade_cara = float(soma_cara)/float(tamanho)*100
+	probabilidade_coroa = float(soma_coroa)/float(tamanho)*100	
+	
+	print 'Foram lancadas ' + str(soma_cara) + ' caras e ' + str(soma_coroa) + ' coroas'
+
+	probabilidades = []
+	probabilidades.append(probabilidade_cara)
+	probabilidades.append(probabilidade_coroa)	
+	
+	return probabilidades
+
+
+matriz=[]
+tamanho = int(raw_input('Digite o tamanho da matriz de lancamentos: '))
+gera_matriz_lancamentos(matriz, tamanho)
+#print 'Um para cara e 2 para coroa'
+#print matriz
+vetor_probabilidades = []
+vetor_probabilidades = calcula_probabilidades(matriz, tamanho)
+print 'As probabilidades sao: %f%% e %f%%' % (vetor_probabilidades[0], vetor_probabilidades[1])
